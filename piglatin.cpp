73f352dbba233dcead4e98f729f572642aa955e5
Austin Hampton <austinhampton914@gmail.com>
2015-10-23 21:44:25 -0700

    Rest of the vowels
    Also b is not a vowel?

--- piglatin.cpp
+++ piglatin.cpp
@@ -8,5 +8,8 @@ int string inputWord = "";
 int string begOutWord = "";
 int string endOutWord = "ay";
 const char VOWEL_A = 'a';
-const char VOWEL_B = 'b';
+const char VOWEL_E = 'e';
+const char VOWEL_U = 'u';
+const char VOWEL_I = 'i';
+const char VOWEL_O = 'o';
 }
