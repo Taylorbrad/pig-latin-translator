a3c1c64c1c37ae2f385b34b114371061a6fa9f01
Taylorbrad <quiero_videojuegos@hotmail.com>
2015-10-23 18:31:35 -0600

    Create piglatin.cpp

---
+++ piglatin.cpp
@@ -0,0 +1,12 @@
+// Example program
+#include <iostream>
+#include <string>
+
+int main()
+{
+int string inputWord = "";
+int string begOutWord = "";
+int string endOutWord = "ay";
+const char VOWEL_A = 'a';
+const char VOWEL_B = 'b';
+}
