f12a29e4652d5596345d7a2fc7eb5cac3e479f18
Taylorbrad <quiero_videojuegos@hotmail.com>
2015-10-26 10:53:29 -0600

    Update piglatin.cpp

--- piglatin.cpp
+++ piglatin.cpp
@@ -1,12 +1,12 @@
 // Pig Latin Translator
 #include <iostream>
 #include <string>
-
+using namespace std;
 int main()
 {
-int string inputWord = "";
-int string begOutWord = "";
-int string endOutWord = "ay";
+char firstVowel = ' ';
+string inputWord = "";
+string begOutWord = "";
 const char VOWEL_A = 'a';
 const char VOWEL_E = 'e';
 const char VOWEL_U = 'u';
@@ -14,8 +14,11 @@ const char VOWEL_I = 'i';
 const char VOWEL_O = 'o';
 
   //create a prompt to ask for the first vowel in the word
-  
-  
+cout << "what word would you like to translate?\n";
+cin >> inputWord;
+cout << "What is the first vowel of that word?\n";
+cin >> firstVowel;
+
   
   
 }
