aa9b9471c76bf2b145f5cf242b257d851d7c910b
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-04-19 10:10:04 -0600

    finished it lmaao

--- piglatin.cpp
+++ piglatin.cpp
@@ -1,33 +1,62 @@
-// Pig Latin Translator
 #include <iostream>
-#include <string>
+
 using namespace std;
-int main()
+int getPositionFirstVowel(string);
+int main(int argc, char *argv[]) 
 {
-//variables and constants
-char firstVowel = ' ';
-string inputWord = "";
-string begOutWord = "";
-
-//possible line saver, given it is correct
-const char VOWELS[] = {'a', 'e', 'u', 'i', 'o',};
-//end saver
-
-const char VOWEL_A = 'a';
-const char VOWEL_E = 'e';
-const char VOWEL_U = 'u';
-const char VOWEL_I = 'i';
-const char VOWEL_O = 'o';
-
-  //user input
-cout << "what word would you like to translate?\n";
-cin >> inputWord;
-cout << "What is the first vowel of that word?\n";
-cin >> firstVowel;
-
-  
-  
+	string wordInput = "";
+	string pigLatinyParts = "";
+	int positionOfFirstVowel = 0;
+	
+	for (int x = 1; x != 0;)
+	{
+		cout << "Input word to translate to pig latin (enter 0 to stop): ";
+		getline(cin, wordInput);
+		positionOfFirstVowel = getPositionFirstVowel(wordInput);
+		
+		if (positionOfFirstVowel == 0) //if first letter is a vowel
+		{
+			wordInput += "-way";
+			cout <<"\n1Pig Latin version:\n" << wordInput << "\n\n";
+		}
+		else if (positionOfFirstVowel > 0) //if there is a vowel in the string, but no the first letter
+		{
+			pigLatinyParts = wordInput.substr(0, (positionOfFirstVowel)) + "ay";
+			wordInput += "-" + pigLatinyParts;
+			wordInput.erase(0, positionOfFirstVowel);
+			cout <<"\n2Pig Latin version:\n" << wordInput << "\n\n";
+		}
+		else if (positionOfFirstVowel == -1) //if there are no vowels in string
+		{
+			if (wordInput == "0")
+			{
+				return 0;
+			}
+			wordInput += "-way";
+			cout << "\n3Pig Latin version:\n" << wordInput << "\n\n";
+		}		
+		
+	}
 }
-
-
-
+int getPositionFirstVowel(string wordInput)
+{
+    string vowels = "aAeEiIoOuU";
+	int testIfVowel = 0;
+	
+	for (int x = 0; x < wordInput.length(); ++x)
+	{
+	    size_t found = vowels.find(wordInput.substr(x, 1));
+	    if (found != string::npos)
+	    {
+	    	//cout << wordInput.substr(0, 1) << "  " << vowels[y] << "\n";
+	   		x += 100;
+		   	--testIfVowel;
+	   	}
+		++testIfVowel;
+	}
+	if (testIfVowel == wordInput.length())
+	{
+		testIfVowel = -1;
+	}
+	return testIfVowel;
+}
